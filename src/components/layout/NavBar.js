2004345fe83c259ad2a588387545db78339a6b76
BatistaMachine <33598815+BatistaMachine@users.noreply.github.com>
2019-12-13 16:01:18 +0100

    add navbar

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -8,7 +8,12 @@ export default class NavBar extends React.Component {
     return (
       <div>
         <nav className="navbar navbar-expand-mb navbar-dark bg-dark fixed-top">
-          <a>Pokedex</a>
+          <a
+            href=""
+            className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center"
+          >
+            Pokedex
+          </a>
         </nav>
       </div>
     );
