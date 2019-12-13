265dbc74177979f0a2ba27c6b6f84ada5c949a3f
BatistaMachine <33598815+BatistaMachine@users.noreply.github.com>
2019-12-13 14:24:18 +0100

    Add navBar

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class NavBar extends React.Component {
   render() {
     return (
       <div>
-        <h1>Alex</h1>
+        <nav className="navbar navbar-expand-mb navbar-dark bg-dark fixed-top">
+          <a>Pokedex</a>
+        </nav>
       </div>
     );
   }
--- src/listing.js
+++ src/listing.js
@@ -1,6 +1,8 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from "react-dom";
 import NavBar from "./components/layout/NavBar.js";
+import "./App.css";
 
 class App extends React.Component {
   render() {
