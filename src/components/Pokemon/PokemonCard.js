986be33deee3e6ec9923b5a8a6498bbe1acacec4
BatistaMachine <33598815+BatistaMachine@users.noreply.github.com>
2019-12-14 10:33:19 +0100

    correction of save the Pokemoncard.js

--- src/components/Pokemon/PokemonCard.js
+++ src/components/Pokemon/PokemonCard.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
+const Sprite = styled.img`
+  width: 5em;
+  height: 5em;
+`;
+
 export default class PokemonCard extends Component {
   state = {
     name: "",
     imageUrl: "",
-    pokemonIndex: ""
+    pokemonIndex: "",
+    imageLoading: true,
+    toManyRequests: false
   };
   componentDidMount() {
     const { name, url } = this.props;
@@ -17,8 +24,23 @@ export default class PokemonCard extends Component {
     return (
       <div className="col-md-3 col-sm-6 mb-5">
         <div className="card">
-          <div className="card-header">
-            <h5>{this.state.pokemonIndex}</h5>
+          <h5 className="card-header">{this.state.pokemonIndex}</h5>
+          <Sprite
+            className="card-img-top rounded mx-auto mt-2"
+            onLoad={() => this.setState({ imageLoading: false })}
+            onError={() => this.setState({ toManyRequests: true })}
+            src={this.state.imageUrl}
+          />
+          <div className="card-body mx-auto">
+            <h6 className="card-title">
+              {this.state.name
+                .toLowerCase()
+                .split(" ")
+                .map(
+                  letter => letter.charAt(0).toUpperCase() + letter.substring(1)
+                )
+                .join(" ")}
+            </h6>
           </div>
         </div>
       </div>
