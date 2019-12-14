6978d57033e15d0903fce87274ed5772974abd28
BatistaMachine <33598815+BatistaMachine@users.noreply.github.com>
2019-12-14 11:05:32 +0100

    loading gif

--- src/components/Pokemon/PokemonCard.js
+++ src/components/Pokemon/PokemonCard.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-
+import spinner from "../Pokemon/245.gif";
 const Sprite = styled.img`
   width: 5em;
   height: 5em;
+  display: none;
 `;
 
 export default class PokemonCard extends Component {
@@ -25,11 +26,25 @@ export default class PokemonCard extends Component {
       <div className="col-md-3 col-sm-6 mb-5">
         <div className="card">
           <h5 className="card-header">{this.state.pokemonIndex}</h5>
+          {this.state.imageLoading ? (
+            <img
+              src={spinner}
+              style={{ width: "10em", height: "1em" }}
+              className="card-img-top rounded mx-auto d-block mt-2"
+            />
+          ) : null}
           <Sprite
             className="card-img-top rounded mx-auto mt-2"
             onLoad={() => this.setState({ imageLoading: false })}
             onError={() => this.setState({ toManyRequests: true })}
             src={this.state.imageUrl}
+            style={
+              this.state.toManyRequests
+                ? { display: "none" }
+                : this.state.imageLoading
+                ? null
+                : { display: "block" }
+            }
           />
           {this.state.toManyRequests ? (
             <h6 className="mx-auto">
